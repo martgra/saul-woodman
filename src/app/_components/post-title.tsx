a4da7dd61822db6055ff1820b25a7c16126a1051
Martin Gran <martgra@gmail.com>
2024-07-12 11:51:19 +0200

    run prettier

--- src/app/_components/post-title.tsx
+++ src/app/_components/post-title.tsx
@@ -1,13 +1,13 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react'
 
 type Props = {
-  children?: ReactNode;
-};
+  children?: ReactNode
+}
 
 export function PostTitle({ children }: Props) {
   return (
-    <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+    <h1 className="mb-12 text-center text-5xl font-bold leading-tight tracking-tighter md:text-left md:text-7xl md:leading-none lg:text-8xl">
       {children}
     </h1>
-  );
+  )
 }
--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,22 +1,22 @@
-import { Metadata } from "next";
-import { notFound } from "next/navigation";
-import { getAllPosts, getPostBySlug } from "@/lib/api";
-import { CMS_NAME } from "@/lib/constants";
-import markdownToHtml from "@/lib/markdownToHtml";
-import Alert from "@/app/_components/alert";
-import Container from "@/app/_components/container";
-import Header from "@/app/_components/header";
-import { PostBody } from "@/app/_components/post-body";
-import { PostHeader } from "@/app/_components/post-header";
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+import { getAllPosts, getPostBySlug } from '@/lib/api'
+import { CMS_NAME } from '@/lib/constants'
+import markdownToHtml from '@/lib/markdownToHtml'
+import Alert from '@/app/_components/alert'
+import Container from '@/app/_components/container'
+import Header from '@/app/_components/header'
+import { PostBody } from '@/app/_components/post-body'
+import { PostHeader } from '@/app/_components/post-header'
 
 export default async function Post({ params }: Params) {
-  const post = getPostBySlug(params.slug);
+  const post = getPostBySlug(params.slug)
 
   if (!post) {
-    return notFound();
+    return notFound()
   }
 
-  const content = await markdownToHtml(post.content || "");
+  const content = await markdownToHtml(post.content || '')
 
   return (
     <main>
@@ -34,23 +34,23 @@ export default async function Post({ params }: Params) {
         </article>
       </Container>
     </main>
-  );
+  )
 }
 
 type Params = {
   params: {
-    slug: string;
-  };
-};
+    slug: string
+  }
+}
 
 export function generateMetadata({ params }: Params): Metadata {
-  const post = getPostBySlug(params.slug);
+  const post = getPostBySlug(params.slug)
 
   if (!post) {
-    return notFound();
+    return notFound()
   }
 
-  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
+  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`
 
   return {
     title,
@@ -58,13 +58,13 @@ export function generateMetadata({ params }: Params): Metadata {
       title,
       images: [post.ogImage.url],
     },
-  };
+  }
 }
 
 export async function generateStaticParams() {
-  const posts = getAllPosts();
+  const posts = getAllPosts()
 
   return posts.map((post) => ({
     slug: post.slug,
-  }));
+  }))
 }
